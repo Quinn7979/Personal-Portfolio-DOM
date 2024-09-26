676752cfc19807be99276ab4b9bddc25254ef9b7
crystal <crysbonnie@gmail.com>
2024-09-26 11:32:23 -0600

    update

--- projects.js
+++ projects.js
@@ -1,1 +1,20 @@
-const projects = [{}, {}, {}];
+const projects = [
+	{
+		title: "My First Project",
+		url: "file:///C:/Users/crystal/Desktop/website/weather%20app/index.html",
+		description:
+			"Is Making a Weather App. I can still use it everyday. All you have to type is City and State. It will give you Date, Temp for a high and a low for that day.",
+	},
+	{
+		title: "My Second Project",
+		url: "file:///C:/Users/crystal/Desktop/website/index.html#page-top",
+		description:
+			"This was Setting my resume up online. Making it all nice with color and icons to see what else i have link. It show my past history, my goals, and what work history.",
+	},
+	{
+		title: "My Third Project",
+		url: "file:///C:/Users/crystal/Documents/GitHub/NumGame/GuessGame",
+		description:
+			"Guessing Game. Where you had to guess the number the computer pick from 1 to 20. Had it set where it was differenet number each time the game played. Would also let you know if your guess was to high or low. Then at the end told you how many times it took for you to guess the correct number.",
+	},
+];
