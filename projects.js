5e8bb418089614c22e57bd60ef97961f94b7e8f3
crystal <crysbonnie@gmail.com>
2024-10-07 12:24:36 -0600

    update

--- projects.js
+++ projects.js
@@ -1,44 +1,58 @@
 const projects = [
 	{
-		title: "My First Project - Weather App",
+		src: "image/weather.png",
+		alt: "weather",
+		title: "Weather App",
 		url: "file:///C:/Users/crystal/Desktop/website/weather%20app/index.html",
 		description:
-			"Its a Weather App. You can still use it everyday. All you have to type is City and State. It will give you Date, Temp for a high and a low for that day.",
+			"The Weather App - You can use it everyday. All you have to type is City and State. It will give you date, temperature for a high and a low for that day.",
 	},
 	{
-		title: "My Second Project - My Resume",
+		src: "image/resume.png",
+		alt: "resume",
+		title: "My Resume",
 		url: "file:///C:/Users/crystal/Desktop/website/index.html#page-top",
 		description:
-			"This is showing my resume online. Making it all nice with color's,  icons, past history, my goals, work history. Share what other sites I use.",
+			"Showing my resume online. Made it all nice with color's,  icons, past history, my goals, work history. Share sites that use.",
 	},
 	{
-		title: "My Third Project - Guessing game",
+		src: "image/guess.png",
+		alt: "guess",
+		title: "Guessing Game- GetHub Link",
 		url: "https://github.com/Quinn7979/NumGame",
 		description:
-			"Guessing Game, you are play with the computer to guess the number the computer pick from 1 to 20. I have set it up where it will be differenet number each time the game played. It will also let you know if your guess was to high or low. Then at the end of the game. It will tell you how many times it took for you to guess the correct number.",
+			"You are play with the computer to guess the number the computer pick from 1 to 20. The number is new each time the game is played. It will tell you how many times it took for you to guess the correct number.",
 	},
 ];
 
-const projectsList = document.getElementById("projects-list");
+const projectsList = document.getElementById("projects-section");
 
 projects.forEach((project) => {
-	const listItem = document.createElement("li");
+	const listItem = document.createElement("div");
 	listItem.className = "project";
+	const pic = document.createElement("img");
+	pic.setAttribute("src", project.src);
+	pic.setAttribute("alt", project.alt);
 
 	const title = document.createElement("h3");
 	title.textContent = project.title || "Untitled Project";
+	title.classList.add("title");
 
 	const description = document.createElement("p");
 	description.textContent = project.description || "No description available.";
+	description.classList.add("description");
+	pic.classList.add("project-img");
 
 	const showUrl = document.createElement("p");
 	const visibleLink = document.createElement("a");
 	visibleLink.href = project.url ? project.url : "#";
 	visibleLink.textContent = "Click here";
+	showUrl.classList.add("link");
 
 	showUrl.appendChild(visibleLink);
 	listItem.appendChild(title);
 	listItem.appendChild(description);
 	listItem.appendChild(showUrl);
 	projectsList.appendChild(listItem);
+	listItem.appendChild(pic);
 });
