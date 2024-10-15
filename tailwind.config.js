ff43fcf18d2f08c7c2fca68a2de7aca845694cea
crystal <crysbonnie@gmail.com>
2024-10-15 11:25:28 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["dashboard.html", "./src/**/*.{html,js}"],
 	theme: {
 		extend: {},
 	},
